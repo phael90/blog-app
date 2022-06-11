fb1e20f153d4dca3db96cb3ddb8254e5f1d39d06
Raphael Ugha <raphael.ugha@gmail.com>
2022-06-11 14:57:07 +0100

    test: having fun with onClick events

--- src/Routes/Home/index.js
+++ src/Routes/Home/index.js
@@ -1,5 +1,18 @@
 const Home = () => {
-  return <header className="home">Homepage</header>;
+
+  const handleClick = (e) => {
+    console.log("Hello Raphael", e);
+  };
+
+  const handleClickAgain = (name) => {
+    console.log("Hi " + name);
+  }
+  return (
+  <header className="home">
+    <h2>Homepage</h2>
+    <button onClick={handleClick}>Click Me</button>
+    <button onClick={() => handleClickAgain("Rapha")}>Click Me Again</button>
+  </header>);
 };
 
 export default Home;
