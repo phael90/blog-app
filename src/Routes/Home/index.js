647786f042d7ff0eb4c313a9a9fcabc52909382c
Raphael Ugha <raphael.ugha@gmail.com>
2022-06-11 16:46:56 +0100

    feat: display blog list

--- src/Routes/Home/index.js
+++ src/Routes/Home/index.js
@@ -1,19 +1,21 @@
 import { useState} from 'react';
 
+let blogDatabase = [
+  {title: "First glog", body: "first", author: "Luigi", id: 1},
+  {title: "Second blog", body: "second", author: "Mario", id: 2},
+  {title: "Third blog", body: "third", author: "Raphael", id: 3},
+];
 const Home = () => {
-  const [name, setName] = useState('mario')
-  const [age, setAge] = useState(33);
+  const [ blogs, setBlog] = useState(blogDatabase);
   
-  const handleClick = () => {
-    setName('luigi');
-    setAge(22);
-  };
-
   return (
   <header className="home">
-    <h2>Homepage</h2>
-    <p>{ name } is {age} years old</p>
-    <button onClick={handleClick}>Click Me</button>
+    {blogs.map((blog) => (
+      <div className="blog-preview" key={blog.id}>
+        <h2>{ blog.title }</h2>
+        <p>Written by { blog.author }</p>
+      </div>
+    ))}
   </header>);
 };
 
