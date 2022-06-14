645f1dc176f28a67d4b681523fcd7e44de13fb41
Raphael Ugha <raphael.ugha@gmail.com>
2022-06-14 14:15:41 +0100

    feat: add error handling for home page

--- src/Routes/Home/index.js
+++ src/Routes/Home/index.js
@@ -4,23 +4,34 @@ import BlogList from '../../BlogList';
 const Home = () => {
   const [ blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
       .then(res => {
+        console.log(res);
+        if(!res.ok){
+          console.log(res);
+          throw Error('erm... sorry we could not fetch the data for that resource ¯\_(ツ)_/¯')
+        }
         return res.json();
       })
       .then((data) => {
-        console.log(data);
         setBlogs(data);
         setIsLoading(false);
-      });
+        setError(null);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setError(error.message);
+      })
     }, 1000)
   }, []);
   
   return (
   <section className="home">
+    { error && <div>{ error }</div>}
     {isLoading && <div>Loading...</div> }
     {blogs && <BlogList blogs={blogs} title="All Blogs" />}
   </section>)
