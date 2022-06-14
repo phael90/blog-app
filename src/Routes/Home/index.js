e43020b076eba7424ea279e6fcf120a0af0808e2
Raphael Ugha <raphael.ugha@gmail.com>
2022-06-14 12:40:57 +0100

    feat: hooked up json db and adde artifical loading

--- src/Routes/Home/index.js
+++ src/Routes/Home/index.js
@@ -2,29 +2,27 @@ import { useState, useEffect} from 'react';
 import BlogList from '../../BlogList';
 
 const Home = () => {
-  const [ blogs, setBlogs] = useState([
-    {title: "First blog", body: "first", author: "luigi", id: 1},
-    {title: "Second blog", body: "second", author: "mario", id: 2},
-    {title: "Third blog", body: "third", author: "mario", id: 3},
-  ]);
-
-  const [name, setName] = useState('mario');
-  
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id )
-    setBlogs(newBlogs);
-  }
+  const [ blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    console.log('useEffect ran');
-    console.log(name);
-  }, [name]);
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+        setIsLoading(false);
+      });
+    }, 1000)
+  }, []);
   
   return (
   <section className="home">
-   <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-   <button onClick={() => setName('luigi')}>change name</button>
-   <p>{ name }</p>
+    {isLoading && <div>Loading...</div> }
+    {blogs && <BlogList blogs={blogs} title="All Blogs" />}
   </section>)
 };
 
