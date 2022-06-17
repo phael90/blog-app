ba68e7129cf859d00e24f78f8cc44a687b2a128d
Raphael Ugha <raphael.ugha@gmail.com>
2022-06-17 14:18:36 +0100

    feat: render blogDetails and styling change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from "./Navbar/NavBar";
 import Home from "./Routes/Home/Home";
 import Create from "./Routes/Create/Create.js";
+import BlogDetails from "./Routes/BlogDetails/BlogDetails";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -16,6 +17,9 @@ function App() {
           </Route>
           <Route path="/create">
             <Create />
+          </Route>
+          <Route path="/blogs/:id">
+            <BlogDetails />
           </Route>
         </Switch>
       </div>
