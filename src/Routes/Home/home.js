b0d06c18e8d378d79520590ba05ca5cd019b9bf2
Raphael Ugha <raphael.ugha@gmail.com>
2022-06-17 13:39:13 +0100

    refactor: used proper html tag name for home component

--- src/Routes/Home/home.js
+++ src/Routes/Home/home.js
@@ -6,11 +6,11 @@ const Home = () => {
   const { data, isLoading, error } = useFetch('http://localhost:8000/blogs')
 
   return (
-  <section className="home">
+  <div className="home">
     {error && <div>{ error }</div>}
     {isLoading && <div>Loading...</div> }
     {data && <BlogList blogs={data} title="All Blogs" />}
-  </section>)
+  </div>)
 };
 
 export default Home;
