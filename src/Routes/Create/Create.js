1d2b1c031d31f2f84f305f27277619e1b8fa6ebd
Raphael Ugha <raphael.ugha@gmail.com>
2022-06-20 00:32:02 +0100

    feat: add POST request to a form

--- src/Routes/Create/Create.js
+++ src/Routes/Create/Create.js
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('yoshi');
+  const [isLoading, setIsLoading] = useState(false);
+  const history = useHistory();
 
  const handleSubmit = (e) => {
    e.preventDefault();
    const blog = { title, body, author }
+   setIsLoading(true);
+
+   fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+   }).then(() => {
+      console.log('new blog aded');
+      setIsLoading(false);
+      history.push('/');
+   })
   }
 
   return (
@@ -35,10 +49,9 @@ const Create = () => {
          <option value="mario">mario</option>
          <option value="yoshi">yoshi</option>
       </select>
-      <button>Add Blog</button>
-      <p>Tracking title:  {title}</p>
-      <p>Tracking body:  {body}</p>
-      <p>Tracking author:  {author}</p>
+      { !isLoading && <button>Add Blog</button> }
+      { isLoading && <button disabled>Adding new blog...</button> }
+
     </form>
  </div>
 )};
