eb865de93ae441212203e77ca9c61cc4a9063f6e
Raphael Ugha <raphael.ugha@gmail.com>
2022-06-19 16:00:20 +0100

    feat: add controlled inputs to form

--- src/Routes/Create/Create.js
+++ src/Routes/Create/Create.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('yoshi');
+
   return (
  <div className="create">
     <h2>Add a New Blog</h2>
+    <form>
+      <label>Blog Title:</label>
+      <input 
+         type="text"
+         required
+         value={title}
+         onChange={(e) => setTitle(e.target.value)}
+      ></input>
+      <label>Blog Body:</label>
+      <textarea 
+         required
+         value={body}
+         onChange={(e) => setBody(e.target.value)}
+      ></textarea>
+      <label>Blog Author:</label>
+      <select
+         value={author}
+         onChange={(e) => setAuthor(e.target.value)}
+      >
+         <option value="mario">mario</option>
+         <option value="yoshi">yoshi</option>
+      </select>
+      <button>Add Blog</button>
+      <p>Tracking title:  {title}</p>
+      <p>Tracking body:  {body}</p>
+      <p>Tracking author:  {author}</p>
+    </form>
  </div>
 )};
 
